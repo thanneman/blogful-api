535cfbc199e0bfd9f19a0450801f8d35cc803e42
thanneman <thanneman1@gmail.com>
2020-01-15 21:31:36 -0700

    Testing GET /articles

--- src/app.js
+++ src/app.js
@@ -17,7 +17,21 @@ app.use(helmet())
 app.use(cors())
 
 app.get('/articles', (req, res, next) => {
-    res.send('All articles')
+    const knexInstance = req.app.get('db')
+    ArticlesService.getAllArticles(knexInstance)
+        .then(articles => {
+            res.json(articles)
+        })
+        .catch(next)
+})
+
+app.get('/articles/:article_id', (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    ArticlesService.getById(knexInstance, req.params.article_id)
+        .then(article => {
+            res.json(article)
+        })
+        .catch(next)
 })
 
 app.get('/', (req, res) => {
