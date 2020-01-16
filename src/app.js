a98a38f97354158f368f30e0f25e1c01bb313cf8
thanneman <thanneman1@gmail.com>
2020-01-15 20:29:06 -0700

    Set up knex

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const ArticlesService = require('./articles-service')
 
 const app = express()
 
@@ -14,6 +15,10 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+
+app.get('/articles', (req, res, next) => {
+    res.send('All articles')
+})
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
