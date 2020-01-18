9af550cee63ed1d1158f78b630ead36c04671ac6
thanneman <thanneman1@gmail.com>
2020-01-18 14:54:01 -0700

    finsihed endpoints/tests for POST and DELETE

--- src/articles/articles-router.js
+++ src/articles/articles-router.js
@@ -1,17 +1,25 @@
 const express = require('express')
+const xss = require('xss')
 const ArticlesService = require('./articles-service')
 
 const articlesRouter = express.Router()
 const jsonParser = express.json()
 
+const serializeArticle = article => ({
+  id: article.id,
+  style: article.style,
+  title: xss(article.title),
+  content: xss(article.content),
+  date_published: article.date_published,
+})
+
 articlesRouter
   .route('/')
   .get((req, res, next) => {
-    ArticlesService.getAllArticles(
-      req.app.get('db')
-    )
+    const knexInstance = req.app.get('db')
+    ArticlesService.getAllArticles(knexInstance)
       .then(articles => {
-        res.json(articles)
+        res.json(articles.map(serializeArticle))
       })
       .catch(next)
   })
@@ -19,13 +27,11 @@ articlesRouter
     const { title, content, style } = req.body
     const newArticle = { title, content, style }
 
-    for (const [key, value] of Object.entries(newArticle)) {
-        if (value == null) {
-            return res.status(400).json({
-                error: { message: `Missing '${key}' in request body`}
-            })
-        }
-    }
+    for (const [key, value] of Object.entries(newArticle))
+      if (value == null)
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` }
+        })
 
     ArticlesService.insertArticle(
       req.app.get('db'),
@@ -35,23 +41,39 @@ articlesRouter
         res
           .status(201)
           .location(`/articles/${article.id}`)
-          .json(article)
+          .json(serializeArticle(article))
       })
       .catch(next)
   })
 
 articlesRouter
   .route('/:article_id')
-  .get((req, res, next) => {
-    const knexInstance = req.app.get('db')
-    ArticlesService.getById(knexInstance, req.params.article_id)
+  .all((req, res, next) => {
+    ArticlesService.getById(
+      req.app.get('db'),
+      req.params.article_id
+    )
       .then(article => {
         if (!article) {
           return res.status(404).json({
             error: { message: `Article doesn't exist` }
           })
         }
-        res.json(article)
+        res.article = article //save the article for the next middleware
+        next() //don't forget to call next so the next middleware happens!
+      })
+      .catch(next)
+  })
+  .get((req, res, next) => {
+    res.json(serializeArticle(res.article))
+  })
+  .delete((req, res, next) => {
+    ArticlesService.deleteArticle(
+      req.app.get('db'),
+      req.params.article_id
+    )
+      .then(() => {
+        res.status(204).end()
       })
       .catch(next)
   })
